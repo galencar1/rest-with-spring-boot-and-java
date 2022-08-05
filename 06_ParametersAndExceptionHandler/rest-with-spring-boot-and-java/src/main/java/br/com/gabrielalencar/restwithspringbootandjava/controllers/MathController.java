306b2bd17f953024f44fdf25a608ec667d5f5503
Gabriel Alencar <gfalencar02@gmail.com>
2022-08-05 17:30:51 -0300

    Definindo metodos de validacao

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/br/com/gabrielalencar/restwithspringbootandjava/controllers/MathController.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/br/com/gabrielalencar/restwithspringbootandjava/controllers/MathController.java
@@ -21,10 +21,18 @@ public class MathController {
     }
 
     private Double convertToDouble(String strNumber) {
-        return null;
+        //Verifica se número é nulo
+        if (strNumber == null) return 0D;
+        //Faz a conversão da moeda. BRL 10,25   US 10.25
+        String number = strNumber.replaceAll(",", ".");
+        //Verifica se valor recebido é numerico e converte pra double
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
     }
 
     private boolean isNumeric(String strNumber) {
-        return false;
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll("," , ".");
+        return number.matches("[+-]?[0-9]*\\.?[0-9]+");
     }
 }
